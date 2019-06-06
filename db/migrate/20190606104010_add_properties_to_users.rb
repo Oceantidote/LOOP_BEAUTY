4095a42b13c8fd821d01fc406147b5a30c61f277
Ben Wright <ben1991wright@hotmail.co.uk>
2019-06-06 11:46:34 +0100

    more attributes

---
+++ db/migrate/20190606104010_add_properties_to_users.rb
@@ -0,0 +1,11 @@
+class AddPropertiesToUsers < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :country, :string
+    add_column :users, :origin, :string
+    add_column :users, :instagram, :string
+    add_column :users, :twitter, :string
+    add_column :users, :youtube, :string
+    add_column :users, :newsletter, :boolean, default: false
+    add_column :users, :accepts_terms, :boolean, default: false
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_06_102819) do
+ActiveRecord::Schema.define(version: 2019_06_06_104010) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -251,6 +251,13 @@ ActiveRecord::Schema.define(version: 2019_06_06_102819) do
     t.datetime "updated_at", null: false
     t.string "first_name"
     t.string "last_name"
+    t.string "country"
+    t.string "origin"
+    t.string "instagram"
+    t.string "twitter"
+    t.string "youtube"
+    t.boolean "newsletter", default: false
+    t.boolean "accepts_terms", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
