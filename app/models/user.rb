48731850049bf6049d63dbb97b6f8c0064de1c45
Ben Wright <ben1991wright@hotmail.co.uk>
2019-06-06 13:27:01 +0100

    add friendly ids for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  include FriendlyId
+  friendly_id :instagram, use: :slugged
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_one :basket
