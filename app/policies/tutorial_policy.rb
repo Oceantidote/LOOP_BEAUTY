0e8a0504168791427bdbbf50f699697d80f55553
Ben Wright <ben1991wright@hotmail.co.uk>
2019-06-06 17:08:04 +0100

    moved policy to right place

--- app/policies/tutorial_policy.rb
+++ app/policies/tutorial_policy.rb
@@ -1,19 +1,7 @@
 class TutorialPolicy < ApplicationPolicy
   class Scope < Scope
     def resolve
-      if user.admin?
-        scope.where(status: 'pending')
-      else
-        raise Pundit::NotAuthorizedError, 'not allowed to view this action'
-      end
+      scope.all
     end
-  end
-
-  def approve?
-    user.admin?
-  end
-
-  def reject?
-    approve?
   end
 end
