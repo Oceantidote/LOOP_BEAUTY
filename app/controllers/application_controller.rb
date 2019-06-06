e068fed5a33a6361031b47cd67ee1d24da4fd1da
Ben Wright <ben1991wright@hotmail.co.uk>
2019-06-06 12:05:04 +0100

    basket methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -34,4 +34,39 @@ class ApplicationController < ActionController::Base
   def skip_pundit?
     devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
   end
+
+  def find_basket
+    if current_user
+      if current_user.basket && session[:basket_id]
+        combine_baskets
+      elsif current_user.basket
+        current_user.basket
+      elsif session[:basket_id]
+        basket = Basket.find(session[:basket_id])
+        session[:basket_id] = nil
+        Basket.update(user: current_user)
+      else
+        Basket.create(user: current_user)
+      end
+    else
+      if session[:basket_id] && Basket.find(session[:basket_id])
+        Basket.find(session[:basket_id])
+      else
+        basket = Basket.create
+        session[:basket_id] = basket.id
+        basket
+      end
+    end
+  end
+
+  def combine_baskets
+    old_basket = Basket.find(session[:basket_id])
+    session[:basket_id] = nil
+    new_basket = current_user.basket
+    old_basket.basket_items.each do |item|
+      item.update(basket: new_basket)
+    end
+    old_basket.destroy
+    new_basket
+  end
 end
