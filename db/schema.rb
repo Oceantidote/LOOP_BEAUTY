722bc9f0faa1b86f8406ee75ae190aa07bd68077
Ben Wright <ben1991wright@hotmail.co.uk>
2019-06-07 14:58:57 +0100

    friendly ids for lookbooks and tutorials

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_07_133555) do
+ActiveRecord::Schema.define(version: 2019_06_07_135732) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -157,6 +157,7 @@ ActiveRecord::Schema.define(version: 2019_06_07_133555) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "title"
+    t.string "slug"
     t.index ["user_id"], name: "index_lookbooks_on_user_id"
   end
 
@@ -278,6 +279,7 @@ ActiveRecord::Schema.define(version: 2019_06_07_133555) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "title"
+    t.string "slug"
     t.index ["user_id"], name: "index_tutorials_on_user_id"
   end
 
