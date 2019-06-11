164ca102c0fb512f20375106b8e7c240d27b87f1
Ife Odugbesan <ifeodugbesan@yahoo.com>
2019-06-11 17:48:45 +0100

    styled insider show page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home, :homepage, :freebies, :insider, :about_us, :questions_and_answers, :influencers, :terms_and_conditions, :shipping, :sale_terms, :privacy_policy]
+  skip_before_action :authenticate_user!, only: [:home, :homepage, :freebies, :insider, :insider_show, :about_us, :questions_and_answers, :influencers, :terms_and_conditions, :shipping, :sale_terms, :privacy_policy]
 
   def home
   end
@@ -11,6 +11,9 @@ class PagesController < ApplicationController
   end
 
   def insider
+  end
+
+  def insider_show
   end
 
   def about_us
