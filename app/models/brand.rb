9ee0c6dffef347f625f7bf9d8dbae7d022830565
Ben Wright <ben1991wright@hotmail.co.uk>
2019-06-17 12:02:46 +0100

    filters now work

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -6,4 +6,9 @@ class Brand < ApplicationRecord
   has_one_attached :photo
   has_one_attached :logo
   validates :name, :logo, :photo, :description, presence: true
+
+  def self.product_filter(ids)
+    return all if ids.empty?
+    where(id: ids)
+  end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,8 @@
 class Category < ApplicationRecord
   has_many :products
+
+  def self.product_filter(ids)
+    return all if ids.empty?
+    where(id: ids)
+  end
 end
