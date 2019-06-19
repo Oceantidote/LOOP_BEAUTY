99fda0af4ba8bab38ade7484408244af626eeec7
Ben Wright <ben1991wright@hotmail.co.uk>
2019-06-19 14:57:07 +0100

    shade cards for products

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -37,40 +37,40 @@ class ApplicationController < ActionController::Base
     devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
   end
 
-  # def find_basket
-  #   if current_user
-  #     if current_user.basket && session[:basket_id]
-  #       combine_baskets
-  #     elsif current_user.basket
-  #       current_user.basket
-  #     elsif session[:basket_id]
-  #       basket = Basket.find(session[:basket_id])
-  #       session[:basket_id] = nil
-  #       Basket.update(user: current_user)
-  #     else
-  #       Basket.create(user: current_user)
-  #     end
-  #   else
-  #     if session[:basket_id] && Basket.find(session[:basket_id])
-  #       Basket.find(session[:basket_id])
-  #     else
-  #       basket = Basket.create
-  #       session[:basket_id] = basket.id
-  #       basket
-  #     end
-  #   end
-  # end
+  def find_basket
+    if current_user
+      if current_user.basket && session[:basket_id]
+        combine_baskets
+      elsif current_user.basket
+        current_user.basket
+      elsif session[:basket_id]
+        basket = Basket.find(session[:basket_id])
+        session[:basket_id] = nil
+        Basket.update(user: current_user)
+      else
+        Basket.create(user: current_user)
+      end
+    else
+      if session[:basket_id] && Basket.find(session[:basket_id])
+        Basket.find(session[:basket_id])
+      else
+        basket = Basket.create
+        session[:basket_id] = basket.id
+        basket
+      end
+    end
+  end
 
-  # def combine_baskets
-  #   old_basket = Basket.find(session[:basket_id])
-  #   session[:basket_id] = nil
-  #   new_basket = current_user.basket
-  #   old_basket.basket_products.each do |item|
-  #     item.update(basket: new_basket)
-  #   end
-  #   old_basket.destroy
-  #   new_basket
-  # end
+  def combine_baskets
+    old_basket = Basket.find(session[:basket_id])
+    session[:basket_id] = nil
+    new_basket = current_user.basket
+    old_basket.basket_products.each do |item|
+      item.update(basket: new_basket)
+    end
+    old_basket.destroy
+    new_basket
+  end
 
   def set_navbar_variables
     @brands = Brand.all.order(name: :ASC)
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,4 @@
 import "bootstrap";
+import initProductShadeCards from "../components/product_shade_cards"
+
+initProductShadeCards()
