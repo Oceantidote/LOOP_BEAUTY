31ab78f54350286723089c35a531333a4e37e509
leonard percival <leonardpercival@gmail.com>
2019-06-20 19:15:24 +0100

    lookbooks controller

--- app/models/lookbook.rb
+++ app/models/lookbook.rb
@@ -6,12 +6,13 @@ class Lookbook < ApplicationRecord
   has_one_attached :photo
   has_many :lookbook_products
   has_many :products, through: :lookbook_products
-
+  validates :title, uniqueness: true
   def approve!
     update(status: 'approved')
   end
 
   def reject!
+
     update(status: 'rejected')
   end
 
