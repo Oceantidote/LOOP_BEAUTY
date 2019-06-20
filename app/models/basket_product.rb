1aedcc4ea7d76ee7cf7ead1db4a44702a4d2f2c3
Ben Wright <ben1991wright@hotmail.co.uk>
2019-06-20 14:31:10 +0100

    order creation sans stripe

--- app/models/basket_product.rb
+++ app/models/basket_product.rb
@@ -2,4 +2,12 @@ class BasketProduct < ApplicationRecord
   belongs_to :basket
   belongs_to :product
   belongs_to :shade
+
+  def convert_to_order_product
+    OrderProduct.new(
+      product: product,
+      shade: shade,
+      quantity: quantity
+    )
+  end
 end
