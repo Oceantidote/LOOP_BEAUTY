d16a71371973c1640169c02b768db0186991fa2b
Ben Wright <ben1991wright@hotmail.co.uk>
2019-07-05 14:16:20 +0100

    add sorting to products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,6 +4,13 @@ class ProductsController < ApplicationController
   def index
     @products = policy_scope(Product)
     @products = Product.filter(params[:product].slice(:category, :brand)) if params[:product].present?
+    if params[:product].present? && params[:product][:sort].present?
+      @products = @products.filter_sort(*sort_params)
+      @sort_method = params[:product][:sort][:method]
+    else
+      @products = @products.filter_sort(*'created_at,desc'.split(','))
+      @sort_method = 'created_at,desc'
+    end
     @categories = Category.all.map { |category| category.name }
     @brand_names = Brand.all.map { |brand| brand.name }
     respond_to do |format|
@@ -19,6 +26,8 @@ class ProductsController < ApplicationController
 
   private
 
-
+  def sort_params
+    params[:product][:sort][:method].split(',')
+  end
 
 end
