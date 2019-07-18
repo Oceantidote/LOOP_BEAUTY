2051259d3925f68eb7fd2e45b7bcc9537feb3aa1
Ben Wright <ben1991wright@hotmail.co.uk>
2019-07-18 16:24:30 +0100

    A! JAX! A! JAX!

--- app/javascript/components/product_shade_cards.js
+++ app/javascript/components/product_shade_cards.js
@@ -6,6 +6,12 @@ const initProductShadeCards = () => {
       $(cards).removeClass('product-shade-card-selected')
       e.currentTarget.classList.add('product-shade-card-selected')
       $('#basket_product_shade_id').val(e.currentTarget.dataset.shadeid)
+      $('.product-carousel').empty()
+      JSON.parse(e.currentTarget.dataset.urls).forEach(url => {
+        $('.product-carousel').append(`<div class="product-shade-image">\
+                                        <img onclick="myFunction(this);" class="item-image-thumbnail" src=${url}>
+                                      </div>`)
+      })
     })
 }
 
