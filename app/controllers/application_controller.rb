8882e849906e8cd67f76efa7b6cdd8c29afe68f3
Ife Odugbesan <ifeodugbesan@yahoo.com>
2019-08-14 15:21:49 +0100

    added phone number to orders and users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -26,10 +26,10 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :country, :origin, :newsletter, :accepts_terms, :dob])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :country, :origin, :newsletter, :accepts_terms, :dob, :phone_number])
 
     # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :country, :origin, :newsletter, :accepts_terms, :dob])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :country, :origin, :newsletter, :accepts_terms, :dob, :phone_number])
   end
 
   def skip_pundit?
