89414627cb531e54906e8356c8c7466454b11844
Ife Odugbesan <ifeodugbesan@yahoo.com>
2019-09-13 10:42:47 +0100

    fix flashes fade out

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -17,3 +17,20 @@ initFlatpickr();
 initTablesorter();
 initStarRating();
 
+const alert = document.querySelector('.alert')
+
+const fadeOut = (flash) => {
+  setTimeout(function() {
+    flash.classList.add('fadeOut')
+    setTimeout(function() {
+      flash.classList.add('hide-flash')
+      }, 500
+    )
+    }, 3000
+  )
+}
+
+if(alert) {
+  fadeOut(alert)
+}
+
