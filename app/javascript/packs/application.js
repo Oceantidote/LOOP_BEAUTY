02fe0e2fdf16f53df76a9bed97a391db43dd3319
Ben Wright <ben1991wright@hotmail.co.uk>
2019-09-17 15:04:27 +0100

    packed up all javascript

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,7 @@ import initCheckout from "../components/checkout";
 import initFlatpickr from "../components/flatpickr";
 import initTablesorter from "../components/tablesorter";
 import initStarRating from '../plugins/init_star_rating';
+import initFadeOut from "../components/fade_out";
 
 initProductShadeCards();
 initMobileFilters();
@@ -16,20 +17,4 @@ initCheckout();
 initFlatpickr();
 initTablesorter();
 initStarRating();
-
-const alert = document.querySelector('.alert')
-
-const fadeOut = (flash) => {
-  setTimeout(function() {
-    flash.classList.add('fadeOut')
-    setTimeout(function() {
-      flash.classList.add('hide-flash')
-      }, 500
-    )
-    }, 3000
-  )
-}
-
-if(alert) {
-  fadeOut(alert)
-}
+initFadeOut();
