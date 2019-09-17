ae583eff19df1fedfd3ae7fa0d0e4aaf8535e5a4
Ben Wright <ben1991wright@hotmail.co.uk>
2019-09-17 13:39:35 +0100

    added second street field for flat numbers, building names, etc.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_11_152518) do
+ActiveRecord::Schema.define(version: 2019_09_17_123244) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 2019_09_11_152518) do
     t.datetime "updated_at", null: false
     t.string "country"
     t.string "county"
+    t.string "street2"
     t.index ["user_id"], name: "index_addresses_on_user_id"
   end
 
