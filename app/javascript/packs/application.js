0b5969359b31f2472abd113b8897d20170d605dc
Ife Odugbesan <ifeodugbesan@yahoo.com>
2019-09-19 10:18:32 +0100

    completed search autocomplete. styling remaining

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,6 +2,7 @@ import "bootstrap";
 import 'flatpickr/dist/flatpickr.min.css';
 import 'flatpickr/dist/themes/dark.css';
 import 'tablesorter/dist/css/theme.default.min.css';
+import "./components/autocomplete";
 
 import initProductShadeCards from "../components/product_shade_cards";
 import initMobileFilters from "../components/mobile_filters";
@@ -10,6 +11,7 @@ import initFlatpickr from "../components/flatpickr";
 import initTablesorter from "../components/tablesorter";
 import initStarRating from '../plugins/init_star_rating';
 import initFadeOut from "../components/fade_out";
+// import autocompleteSearch from "../components/autocomplete"
 
 initProductShadeCards();
 initMobileFilters();
@@ -18,3 +20,4 @@ initFlatpickr();
 initTablesorter();
 initStarRating();
 initFadeOut();
+// autocompleteSearch();
