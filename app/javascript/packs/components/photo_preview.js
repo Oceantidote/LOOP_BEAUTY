a060f869054a719b88801a9057383561fc5185ba
Ben Wright <ben1991wright@hotmail.co.uk>
2019-10-07 16:21:16 +0100

    rewrite image preview JS to fix bug on admin product

--- app/javascript/packs/components/photo_preview.js
+++ app/javascript/packs/components/photo_preview.js
@@ -1,28 +1,28 @@
 displayPreview = (input) => {
   videoPreview = document.querySelector('.video-preview')
   if (input.files && input.files[0]) {
-    let reader = new FileReader();
-    document.querySelectorAll('.photo-preview').forEach((preview) => {
-      if (input.dataset.name === preview.dataset.name) {
-        reader.onload = (event) => {
-          if (event.currentTarget.result.substring(0,10) === "data:image") {
-            console.log("true")
-            if (videoPreview) {
-              videoPreview.remove();
-            }
-            preview.src = event.currentTarget.result;
-          } else {
-            console.log("false")
-            if (videoPreview) {
-              videoPreview.remove();
-            }
-            preview.src = ''
-            input.insertAdjacentHTML('afterend', `<div class="video-preview photo-preview" data-name="tutorial-video"><p class="boldest no-margin didot">${input.files[0].name}</p></div>`)
+    let previews = input.parentNode.parentNode.querySelectorAll('.photo-preview')
+    Array.from(input.files).forEach(file => {
+      let reader = new FileReader();
+      reader.onload = (event) => {
+        if (event.currentTarget.result.substring(0,10) === "data:image") {
+          console.log("true")
+          if (videoPreview) {
+            videoPreview.remove();
           }
+          if (previews.length > 1) {
+              previews[previews.length - 1].insertAdjacentHTML('afterend', `<img height="100" class="photo-preview" data-name="shade-photos" alt="shade photo" src="${event.currentTarget.result}">`)
+          } else {
+            previews.forEach(preview => preview.src = event.currentTarget.result)
+          }
+        } else {
+          console.log("false")
+          preview.src = ''
+          input.insertAdjacentHTML('afterend', `<div class="video-preview photo-preview" data-name="tutorial-video"><p class="boldest no-margin didot">${input.files[0].name}</p></div>`)
         }
-        reader.readAsDataURL(input.files[0])
-        preview.classList.remove('d-none');
       }
+      reader.readAsDataURL(file)
     })
+    previews.forEach(preview => preview.classList.remove('d-none'))
   }
 }
