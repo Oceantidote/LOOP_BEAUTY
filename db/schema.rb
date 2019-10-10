f512cb75bcecaaec2013c740899ae9e56ec33c3e
Ife Odugbesan <ifeodugbesan@yahoo.com>
2019-10-10 12:32:51 +0100

    added DOB to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_08_101142) do
+ActiveRecord::Schema.define(version: 2019_10_10_105814) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -432,6 +432,7 @@ ActiveRecord::Schema.define(version: 2019_10_08_101142) do
     t.float "commission_rate", default: 0.0
     t.string "instagram_followers"
     t.string "youtube_subscribers"
+    t.string "gender"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["slug"], name: "index_users_on_slug", unique: true
