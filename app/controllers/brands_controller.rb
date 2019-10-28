51e9fe468fb1457b5262429f2ff0b3eb953b9626
Ife Odugbesan <ifeodugbesan@yahoo.com>
2019-10-28 10:23:19 +0000

    fixed pagination to work on an array

--- app/controllers/brands_controller.rb
+++ app/controllers/brands_controller.rb
@@ -11,12 +11,12 @@ class BrandsController < ApplicationController
     @products = @brand.products
     @original = @brand.products
     @start = params[:product].nil? || params[:product][:category].reject(&:blank?).empty?
-    @products = @products.filter(params[:product].slice(:category, :brand)) if params[:product].present?
+    @products = @products.filter(params[:product].slice(:category, :brand)).page params[:page] if params[:product].present?
     if params[:product].present? && params[:product][:sort].present?
       @products = @products.filter_sort(*sort_params)
       @sort_method = params[:product][:sort][:method]
     else
-      @products = @products.filter_sort(*'created_at,desc'.split(','))
+      @products = @products.filter_sort(*'created_at,desc'.split(',')).page params[:page]
       @sort_method = 'created_at,desc'
     end
     respond_to do |format|
