3cab8550c839685f5928c508d80e2a0e930d84fe
Ife Odugbesan <ifeodugbesan@yahoo.com>
2019-11-13 13:31:39 +0000

    fixed bugs for ipad layout

--- app/javascript/packs/components/photo_preview.js
+++ app/javascript/packs/components/photo_preview.js
@@ -2,7 +2,7 @@ displayPreview = (input) => {
   videoPreview = document.querySelector('.video-preview')
   if (input.files && input.files[0]) {
     let preview = input.parentNode.parentNode.querySelector('.photo-preview')
-    console.log(preview.dataset)
+    // console.log(preview.dataset)
     Array.from(input.files).forEach(file => {
       let reader = new FileReader();
       reader.onload = (event) => {
@@ -17,12 +17,16 @@ displayPreview = (input) => {
           }
         } else {
           console.log("false")
-          preview.src = ''
+          if (preview) {
+            preview.src = ''
+          }
           input.insertAdjacentHTML('afterend', `<div class="video-preview photo-preview" data-name="tutorial-video"><p class="boldest no-margin didot">${input.files[0].name}</p></div>`)
         }
       }
       reader.readAsDataURL(file)
-      preview.classList.remove('d-none')
+      if (preview) {
+        preview.classList.remove('d-none')
+      }
     })
   }
 }
