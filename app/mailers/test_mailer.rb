16cf1c10314464a6ee7527301063a55eab978e4c
Ife Odugbesan <ifeodugbesan@yahoo.com>
2020-01-04 10:48:31 +0000

    Added Ariel's changes. Updated policies and email addresses

--- app/mailers/test_mailer.rb
+++ app/mailers/test_mailer.rb
@@ -2,8 +2,8 @@ class TestMailer < ActionMailer::Base
   def hello
     mail(
       :subject => 'Hello from Postmark',
-      :to  => 'hello@loopbeauty.co.uk',
-      :from => 'hello@loopbeauty.co.uk',
+      :to  => 'hello@myloopbeauty.com',
+      :from => 'hello@myloopbeauty.com',
       :html_body => '<strong>Hello</strong> dear Postmark user.',
       :track_opens => 'true')
   end
