95ca6018b08c389b6b4d8e9c3fba679625b85735
Ife Odugbesan <ifeodugbesan@yahoo.com>
2020-02-28 14:02:20 +0000

    added select 2 to new in

--- app/javascript/plugins/init_select2.js
+++ app/javascript/plugins/init_select2.js
@@ -4,6 +4,7 @@ import 'select2'
 const initSelect2 = () => {
   $('#lookbook_product_ids').select2()
   $('#tutorial_product_ids').select2()
+  $('#new_in_products').select2()
 }
 
 export default initSelect2
