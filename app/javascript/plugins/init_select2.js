a8a2f19f2f20d0b0fdc65a018f2ae81046212c6f
Ife Odugbesan <ifeodugbesan@yahoo.com>
2020-03-03 14:48:39 +0000

    updated the old loop logo

--- app/javascript/plugins/init_select2.js
+++ app/javascript/plugins/init_select2.js
@@ -6,6 +6,7 @@ const initSelect2 = () => {
   $('#tutorial_product_ids').select2()
   $('#new_in_products').select2()
   $('#tutorial_user').select2()
+  $('#showroom_product_product').select2()
 }
 
 export default initSelect2
