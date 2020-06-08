ea1aa9a418184d687dbf36766f1c14a6a8781ba9
leonard percival <leonardpercival@gmail.com>
2020-06-08 12:54:01 +0100

    remove ife

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -52,7 +52,7 @@ class UserMailer < ApplicationMailer
     @rejected = params[:rejected]
     if @rejected
       mail(to: 'hello@myloopbeauty.com', subject: "#{@influencer.full_name} has edited a rejected #{@content.class.to_s}" )
-      mail(to: 'ifeodugbesan@hoxton-digital.com', subject: "#{@influencer.full_name} has edited a rejected #{@content.class.to_s}" )
+      # mail(to: 'ifeodugbesan@hoxton-digital.com', subject: "#{@influencer.full_name} has edited a rejected #{@content.class.to_s}" )
     else
       mail(to: 'hello@myloopbeauty.com', subject: "#{@influencer.full_name} has shared a new #{@content.class.to_s}" )
       # mail(to: 'ifeodugbesan@hoxton-digital.com', subject: "#{@influencer.full_name} has shared a new #{@content.class.to_s}" )
