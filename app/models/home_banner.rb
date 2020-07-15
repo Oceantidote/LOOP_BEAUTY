fa691df0d9fa3f222576783ecf8f498d520e710c
leonard percival <leonardpercival@gmail.com>
2020-07-15 09:58:25 +0100

    allow jpeg

--- app/models/home_banner.rb
+++ app/models/home_banner.rb
@@ -10,18 +10,18 @@ class HomeBanner < ApplicationRecord
   has_one_attached :galaxy
   validates :banner, attached: true
   validates :mobile_banner, attached: true
-  validates :ipad, attached: true, content_type: ['image/png', 'image/jpg'],
+  validates :ipad, attached: true, content_type: ['image/png', 'image/jpg', 'image/jpeg'],
                                      dimension: { width: 768, height: 924 }
-  validates :ipadpro, attached: true, content_type: ['image/png', 'image/jpg'],
+  validates :ipadpro, attached: true, content_type: ['image/png', 'image/jpg', 'image/jpeg'],
                                      dimension: { width: 1024, height: 1266 }
-  validates :ipadx, attached: true, content_type: ['image/png', 'image/jpg'],
+  validates :ipadx, attached: true, content_type: ['image/png', 'image/jpg', 'image/jpeg'],
                                      dimension: { width: 375, height: 712 }
-  validates :iphone, attached: true, content_type: ['image/png', 'image/jpg'],
+  validates :iphone, attached: true, content_type: ['image/png', 'image/jpg', 'image/jpeg'],
                                      dimension: { width: 375, height: 567 }
-  validates :iphonese, attached: true, content_type: ['image/png', 'image/jpg'],
+  validates :iphonese, attached: true, content_type: ['image/png', 'image/jpg', 'image/jpeg'],
                                      dimension: { width: 320, height: 468 }
-  validates :pixel, attached: true, content_type: ['image/png', 'image/jpg'],
+  validates :pixel, attached: true, content_type: ['image/png', 'image/jpg', 'image/jpeg'],
                                      dimension: { width: 411, height: 631 }
-  validates :galaxy, attached: true, content_type: ['image/png', 'image/jpg'],
+  validates :galaxy, attached: true, content_type: ['image/png', 'image/jpg', 'image/jpeg'],
                                      dimension: { width: 360, height: 540 }
 end
