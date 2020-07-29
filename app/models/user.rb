698d3e8d99401613348e711366ea7f3e089976e8
leonard percival <leonardpercival@gmail.com>
2020-07-29 18:18:23 +0100

    makeup cover photos

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,6 +19,9 @@ class MyValidator < ActiveModel::Validator
       unless record.tutorial_photo.attached?
         record.errors[:tutorial_photo] << 'must be provided'
       end
+      unless record.makeup_cover_photo.attached?
+        record.errors[:makeup_cover_photo] << 'must be provided'
+      end
       unless record.makeup_photo.attached?
         record.errors[:makeup_photo] << 'must be provided'
       end
@@ -55,6 +58,7 @@ class User < ApplicationRecord
   has_one_attached :lookbook_photo
   has_one_attached :tutorial_photo
   has_one_attached :makeup_photo
+  has_one_attached :makeup_cover_photo
   has_many :lookbooks, dependent: :destroy
   has_one :question_answer, dependent: :destroy
   has_many :customer_reviews, dependent: :destroy
