4ed888ea1fe079664cbe2524a0b42bdd6f55880f
leonard percival <leonardpercival@gmail.com>
2020-08-06 11:29:05 +0100

    ready for new staging push

--- app/models/home_banner.rb
+++ app/models/home_banner.rb
@@ -8,9 +8,18 @@ class HomeBanner < ApplicationRecord
   has_one_attached :iphone_8_plus
   has_one_attached :iphone_8
   has_one_attached :ipad_pro
+  has_one_attached :ipad_11
+  has_one_attached :ipad_7
+  has_one_attached :ipad_air
   has_one_attached :ipad_third__fourth_generation
-  has_one_attached :nexus_6p
+  has_one_attached :nexus_6p_pixel_1_2
   has_one_attached :google_pixel_2_xl
+  has_one_attached :google_pixel_3_xl
+  has_one_attached :google_pixel_3a_xl
+  has_one_attached :googel_pixel_4_xl
+  has_one_attached :pixel_3a
+  has_one_attached :pixel_3
+  has_one_attached :pixel_4
   has_one_attached :samsung_galaxy_note_5
   has_one_attached :samsung_galaxy_s9
   has_one_attached :samsung_galaxy_s7
@@ -19,7 +28,7 @@ class HomeBanner < ApplicationRecord
   has_one_attached :chromebook_pixel
 
   UNIQUE_VIEWPORTS.each do |viewport|
-    validates viewport.first, content_type: ['image/png', 'image/jpg', 'image/jpeg'], dimension: { width: viewport.second[:width], height: viewport.second[:height] - 100}
+    validates viewport.first, content_type: ['image/png', 'image/jpg', 'image/jpeg'], dimension: { width: viewport.second[:width], height: viewport.second[:height] - viewport.second[:browser]}
   end
 
   def am_i_valid?
