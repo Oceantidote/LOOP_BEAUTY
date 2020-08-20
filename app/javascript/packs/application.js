0ecb5191f98c7b89f4867b75c9bd825746e36312
Ben Wright <ben1991wright@hotmail.co.uk>
2020-08-20 12:21:07 +0100

    add button that displays when card hovered on

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,77 +1,86 @@
 import $ from 'jquery'
 global.$ = $
 
-import 'bootstrap';
+import 'bootstrap'
 import 'chartkick'
 import 'chart.js'
-import 'flatpickr/dist/flatpickr.min.css';
-import 'flatpickr/dist/themes/dark.css';
-import 'tablesorter/dist/css/theme.default.min.css';
+import 'flatpickr/dist/flatpickr.min.css'
+import 'flatpickr/dist/themes/dark.css'
+import 'tablesorter/dist/css/theme.default.min.css'
 import 'select2/dist/css/select2.min.css'
 
-import "./components/cookie_box";
-import "./components/autocomplete";
+import './components/cookie_box'
+import './components/autocomplete'
 
-import initProductShadeCards from "../components/product_shade_cards";
-import initMobileFilters from "../components/mobile_filters";
-import initCheckout from "../components/checkout";
-import initFlatpickr from "../components/flatpickr";
-import initTablesorter from "../components/tablesorter";
-import initStarRating from '../plugins/init_star_rating';
-import initFadeOut from "../components/fade_out";
-import { initSweetalert } from '../plugins/init_sweetalert';
-import initSelect2 from '../plugins/init_select2';
-import 'slick-carousel';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
+import initProductShadeCards from '../components/product_shade_cards'
+import initMobileFilters from '../components/mobile_filters'
+import initCheckout from '../components/checkout'
+import initFlatpickr from '../components/flatpickr'
+import initTablesorter from '../components/tablesorter'
+import initStarRating from '../plugins/init_star_rating'
+import initFadeOut from '../components/fade_out'
+import { initSweetalert } from '../plugins/init_sweetalert'
+import initSelect2 from '../plugins/init_select2'
+import 'slick-carousel'
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
+import { initProductCards } from '../components/product_cards'
 // import autocompleteSearch from "../components/autocomplete"
 
-initProductShadeCards();
-initMobileFilters();
-initCheckout();
-initFlatpickr();
-initTablesorter();
-initStarRating();
-initFadeOut();
+initProductShadeCards()
+initMobileFilters()
+initCheckout()
+initFlatpickr()
+initTablesorter()
+initStarRating()
+initFadeOut()
 initSelect2()
+initProductCards()
 // autocompleteSearch();
-
 
 // ========================
 //  Sweet Alert for Deleting Addresses
 // ========================
 
 // Delivery Adresses
-initSweetalert('.sweet-alert-delivery-trigger', {
-  title: 'Are you sure?',
-  text: "You won't be able to revert this!",
-  icon: 'warning',
-}, (value) => {
-  if (value) {
-    const link = document.querySelector(`#delete-delivery-link${window.deliveryId}`);
-    link.click();
+initSweetalert(
+  '.sweet-alert-delivery-trigger',
+  {
+    title: 'Are you sure?',
+    text: "You won't be able to revert this!",
+    icon: 'warning',
+  },
+  (value) => {
+    if (value) {
+      const link = document.querySelector(`#delete-delivery-link${window.deliveryId}`)
+      link.click()
+    }
   }
-});
+)
 
 // Billing Adresses
-initSweetalert('.sweet-alert-billing-trigger', {
-  title: 'Are you sure?',
-  text: "You won't be able to revert this!",
-  icon: 'warning',
-}, (value) => {
-  if (value) {
-    const link = document.querySelector(`#delete-billing-link${window.billingId}`);
-    link.click();
+initSweetalert(
+  '.sweet-alert-billing-trigger',
+  {
+    title: 'Are you sure?',
+    text: "You won't be able to revert this!",
+    icon: 'warning',
+  },
+  (value) => {
+    if (value) {
+      const link = document.querySelector(`#delete-billing-link${window.billingId}`)
+      link.click()
+    }
   }
-});
+)
 
 // // ========================
 // //  Slick
 // // ========================
 
-const carousel = document.querySelector('.home-featured-container');
+const carousel = document.querySelector('.home-featured-container')
 if (carousel) {
-  $(document).ready(function(){
+  $(document).ready(function () {
     $('.homepage-slick').slick({
       infinite: true,
       slidesToShow: 3,
@@ -85,18 +94,16 @@ if (carousel) {
         {
           breakpoint: 992,
           settings: {
-            slidesToShow: 2
-          }
+            slidesToShow: 2,
+          },
         },
         {
           breakpoint: 576,
           settings: {
-            slidesToShow: 1
-          }
-        }
-      ]
-    });
-  });
+            slidesToShow: 1,
+          },
+        },
+      ],
+    })
+  })
 }
-
-
