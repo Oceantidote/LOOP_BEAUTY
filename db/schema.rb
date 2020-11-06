92d46e66b3ad86a37a3cfa631a578171eca80aaf
leonard percival <leonardpercival@gmail.com>
2020-11-06 16:45:16 +0000

    basket index

--- db/schema.rb
+++ db/schema.rb
@@ -324,7 +324,6 @@ ActiveRecord::Schema.define(version: 2020_11_06_143140) do
     t.boolean "demoable", default: false
     t.boolean "published", default: false
     t.boolean "featured", default: false
-    t.integer "us_price_cents", default: 0
     t.index ["brand_id"], name: "index_products_on_brand_id"
     t.index ["category_id"], name: "index_products_on_category_id"
     t.index ["department_id"], name: "index_products_on_department_id"
@@ -370,9 +369,6 @@ ActiveRecord::Schema.define(version: 2020_11_06_143140) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "number_in_stock", default: 0
-    t.integer "us_number_in_stock", default: 0
-    t.boolean "uk_available", default: true
-    t.boolean "us_available", default: false
     t.index ["product_id"], name: "index_shades_on_product_id"
   end
 
