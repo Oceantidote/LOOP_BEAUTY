8baa6be0b06ad7d08c04fcbf508a1f8e86bba425
Ben Wright <ben1991wright@hotmail.co.uk>
2020-12-17 15:54:23 +0000

    fix image upload

--- app/jobs/add_images_to_mailchimp_products_job.rb
+++ app/jobs/add_images_to_mailchimp_products_job.rb
@@ -4,7 +4,11 @@ class AddImagesToMailchimpProductsJob < ApplicationJob
   def perform
     client = MailchimpMarketing::Client.new(api_key: ENV['MAILCHIMP_API_KEY'], server: 'us19')
     Product.find_each do |product|
-      url = product.lead_product_image.attached? ? product.lead_product_image.blob.service_url : product.shades.first.photos.first.blob.service_url
+      url = product.lead_product_image.attached? ? product.lead_product_image.blob.service_url : product.shades.first.photos.first&.blob&.service_url
+      if url.nil?
+        puts "Skipping #{product.title} - #{product.id}"
+        next
+      end
       client.ecommerce.update_store_product(ENV['MAILCHIMP_STORE_ID'], product.id.to_s, {
         image_url: url
       })
